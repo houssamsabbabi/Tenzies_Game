89c1e64352bf9e30729903d6d2c878606befc4c8
houssam Sabbabi <houssamfsdm@gmail.com>
2022-11-21 13:06:34 +0100

    Updates

---
+++ precache-manifest.7b54cffa5365f3b8e14df2b1afd37df2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbfd41ade665f4a7fc68",
+    "url": "/Tenzies_game/static/css/main.a8150a9c.chunk.css"
+  },
+  {
+    "revision": "cbfd41ade665f4a7fc68",
+    "url": "/Tenzies_game/static/js/main.cbfd41ad.chunk.js"
+  },
+  {
+    "revision": "4727ec555ec479f7474c",
+    "url": "/Tenzies_game/static/js/1.4727ec55.chunk.js"
+  },
+  {
+    "revision": "724d1377f10d34ea3e0f",
+    "url": "/Tenzies_game/static/js/runtime~main.724d1377.js"
+  },
+  {
+    "revision": "4465c1266553fedfadf7c20f7e917c0b",
+    "url": "/Tenzies_game/index.html"
+  }
+];
